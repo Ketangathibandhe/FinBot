c79d7b342189ee4815bc2afa2095a99a3d7a9cdb
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-12-02 16:37:38 +0530

    Expense model

---
+++ src/models/Expense.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+  // Linking from User table 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String, // like "Momos", "Petrol"
+    required: true
+  },
+  amount: {
+    type: Number, //  100, 500
+    required: true
+  },
+  category: {
+    type: String,
+    default: 'General' // Food, Travel, etc.
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Expense', expenseSchema);
