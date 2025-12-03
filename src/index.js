af4f7bb36dd0ecea37692802e2f3baa30d649719
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-12-03 19:56:12 +0530

    JWT and cookie-parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const cookieParser = require('cookie-parser')
 const { connectDB } = require("./config/database");
 
 // Config
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("FinBot Server is Running!");
