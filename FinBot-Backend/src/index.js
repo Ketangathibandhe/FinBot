ea2239f43c8b1d1dc2197ad004c9a6640b01614b
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-12-31 20:13:51 +0530

    Add pre-flight handling for CORS options

--- FinBot-Backend/src/index.js
+++ FinBot-Backend/src/index.js
@@ -48,7 +48,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 )
-
+app.options('*', cors()); //Force Pre-flight handling
 app.use(express.json());
 app.use(cookieParser());
 
